a3c0a05c5ce3a350110527426d1a7f37bc43298d
FooB4r <bambambambooboo@gmail.com>
2018-04-11 11:47:51 +0100

    Dummy server ok request -> respond OK

--- test_server.ml
+++ test_server.ml
@@ -16,13 +16,13 @@ let eq_http response_str expected =
     | _ -> false
   )
 
-(* Nothing ATM *)
-let server = ""
+(* Simple server that only returns the message OK when ok and nothing otherwise *)
+let server =
+  let callback _conn req body =
+    body |> Cohttp_lwt.Body.to_string >>= (fun body ->
+      Server.respond_string ~status:`OK ~body ()
+    )
+  in
+  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
 
-let get_reponse response_str =
-  String_io.M.(
-    StringResponse.read (String_io.open_in response_str)
-    >>= function
-    | `Ok resp -> failwith "whatever"
-    | _ -> failwith ("### Fail on: ###\n" ^ response_str)
-  )
+let () = ignore (Lwt_main.run server)
