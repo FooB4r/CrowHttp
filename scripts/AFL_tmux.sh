620225bac92438f5c81767c43cdcc15808433e6c
FooB4r <bambambambooboo@gmail.com>
2018-04-30 14:38:13 +0100

    Simpler tmux using binds

--- scripts/AFL_tmux.sh
+++ scripts/AFL_tmux.sh
@@ -5,18 +5,6 @@ set -eu
 aflfuzz=afl-fuzz
 executable=_build/default/main.exe
 
-# echo $core_pattern_backup >/proc/sys/kernel/core_pattern
-# echo $scaling_governor_backup | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-
-# Reset on exit
-exit_tmux="on_exit() {
-  tmux kill-session -t afl_fuzz
-  exit
-}
-
-trap 'on_exit' INT
-"
-
 ## parallelization
 cmd1="$aflfuzz -i input/ -o sync_dir/ -M fuzzer01 $executable"
 cmd2="$aflfuzz -i input/ -o sync_dir/ -S fuzzer02 $executable"
@@ -24,12 +12,13 @@ cmd3="$aflfuzz -i input/ -o sync_dir/ -S fuzzer03 $executable"
 cmd4="$aflfuzz -i input/ -o sync_dir/ -S fuzzer04 $executable"
 
 tmux new -s afl_fuzz -d
+tmux bind-key -n C-c send-keys C-c "tmux kill-session -t afl_fuzz" Enter
 tmux split-window -v -t afl_fuzz:0.0
 tmux split-window -h -t afl_fuzz:0.0
 tmux split-window -h -t afl_fuzz:0.2
 # Fuzzers
-tmux send-keys -t afl_fuzz:0.0 "$exit_tmux $cmd1" C-m
-tmux send-keys -t afl_fuzz:0.1 "$exit_tmux $cmd2" C-m
-tmux send-keys -t afl_fuzz:0.2 "$exit_tmux $cmd3" C-m
-tmux send-keys -t afl_fuzz:0.3 "$exit_tmux $cmd4" C-m
+tmux send-keys -t afl_fuzz:0.0 "$cmd1" C-m
+tmux send-keys -t afl_fuzz:0.1 "$cmd2" C-m
+tmux send-keys -t afl_fuzz:0.2 "$cmd3" C-m
+tmux send-keys -t afl_fuzz:0.3 "$cmd4" C-m
 tmux attach -t afl_fuzz
